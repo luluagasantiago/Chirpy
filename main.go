e20e3931c676200304d03b4627b401194fdadec6
luluagasantiago <luluagasantiago@icloud.com>
2024-01-29 22:44:53 +0100

    count requests added

--- main.go
+++ main.go
@@ -1,32 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 	//look: serveMux implements the interface Handler ()
+	apcfg := &apiConfig{fileserverHits: 0}
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", apcfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		//w.WriteHeader(200)
+		w.Write([]byte(http.StatusText(200)))
+
+	})
+	mux.HandleFunc("/metrics", apcfg.middlewareCountRequests)
+	mux.HandleFunc("/reset", apcfg.middlewareResetCounts)
+
 	// mux is a hanlder bc it has an implementation of the ServeHTTP function
 	corsMux := middlewareCors(mux) //
-	// http.HandlerFunc -> type HandlerFunc func(ResponseWriter, *Request)
+	// http.HandlerFunc -> type HandlerFunc func(ResponseWriter, *Request) -> Handler interface
 	server := http.Server{
+		Addr:    ":" + port,
 		Handler: corsMux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 
-	// by default listens to port 80
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
-
 }
 
 /*
-ListenAndServe starts an HTTP server with a given address and handler.
- The handler is usually nil, which means to use DefaultServeMux.
-  Handle and HandleFunc add handlers to DefaultServeMux:
-
-
 #What is a Handler?
 
 A Handler responds to an HTTP request.
@@ -35,33 +43,9 @@ type Handler interface {
 	ServeHTTP(ResponseWriter, *Request)
 }
 
-ServeHTTP should write reply headers and data to the ResponseWriter and then return.
-Returning signals that the request is finished; it is not valid to use the ResponseWriter or read from the Request.
-Body after or concurrently with the completion of the ServeHTTP call.
-
-*/
-
-/*
-
-type Handler interface {
-	ServeHTTP(ResponseWriter, *Request)
-}
 */
 
 func middlewareCors(next http.Handler) http.Handler {
-	/*
-		The HandlerFunc type is an adapter to allow the use
-		of ordinary functions as HTTP handlers.
-		 If f is a function with the appropriate signature,
-		 -> HandlerFunc(f) is a Handler that calls f.
-
-		- type HcandlerFunc func(ResponseWriter, *Request)
-
-	*/ //---------
-	// The HandlerFunc type is an adapter to allow the use of
-	// ordinary functions as HTTP handlers. If f is a function
-	// with the appropriate signature, HandlerFunc(f) is a
-	// Handler that calls f.
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//this wrapper sets the headers for the ResponseWriter
 		// and calls ServeHTTP(w,r)
@@ -74,6 +58,34 @@ func middlewareCors(next http.Handler) http.Handler {
 		}
 		next.ServeHTTP(w, r)
 		//ServeHTTP dispatches the request to the handler
-		//whose pattern most closely matches the request URL.
+		//whose pattern most closely matches the request URL from request.
 	})
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		fmt.Println("HOLA")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareCountRequests(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	//w.WriteHeader(200)
+	fmt.Println("HOLA1")
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits)))
+
+}
+
+func (cfg *apiConfig) middlewareResetCounts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits = 0
+	w.WriteHeader(200)
+	fmt.Println("HOLA")
+
 }
